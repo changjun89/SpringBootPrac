bd2e3386631742f2101f73fd04adf2063c2074ff
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2018-12-19 22:05:08 +0900

    initial commit

---
+++ src/main/java/com/jojoldu/webservice/web/WebRestController.java
@@ -0,0 +1,29 @@
+package com.jojoldu.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
+import com.jojoldu.webservice.service.PostsService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsService postsService;
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
+    }
+}
